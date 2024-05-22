f4a5a5dc84728ffd65abe245e0e088ae3a38563a
Anne-Mari Kasemetsa <annemari.kasemetsa3@gmail.com>
2024-05-22 16:23:08 +0300

    Algne fail

---
+++ main.py
@@ -0,0 +1,26 @@
+## Projekti kirjeldus
+
+#Näiteks: sisestad inimesed kelle vahel tšekk jagada ja saad valida kellele mingi rida
+# või alamkategooria jagada. Lisaks toodetele on võimalik kogu tšekki protsentidega ka jagada.
+# Peale ära jagamist tagastatakse ülevaatlik koondtulemus tekstina, mida edasi oma sõpradele jagada,
+# et raha tagasi saada.
+
+#Peame leidma viisi kuidas kõik vajalikud read ja ainult vajalikud read pildilt kätte saada.
+# Erinevate poodide tšekid on erineva kujundusega.
+# Lisaks peab pilti kindlasti töötlema valguse ning positsiooni osas.
+# Inimese ja arvuti suhtluseks on tarvis luua juturoboti taoline asi.
+# Selle jaoks on vaja luua tekst ning vastustele vastavalt küsimused ja tagastused.
+
+# 1. Sisestad pildi
+# 2. võibolla vaja image töötlust
+# 2. programm loeb sisse pildi ning küsib, mida teha (kuidas jagada)
+# 3. programm loeb pildi pealt tooted ning nende hinnad
+# 4. vastavalt kasutaja soovile jaotab programm automaatselt ära, kes kui palju peab maksma
+#
+# pildi pealt character ja sõnade recognition - tesseract
+#
+# juturobot, küsib mida teha ning kui ei tea täpselt mida teha siis küsib üle
+#
+# Et pildilt arusaada, mis toodet kuhu jaotada, peab inimene ütlema tšeki pealt kas toote järjekorranumbri või siis toote nimest vähemalt mingi osa.
+#
+# Kui juturobot ei leia täpselt, siis küsib üle, kuni saab õige
