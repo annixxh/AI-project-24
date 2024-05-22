82aecb04eed2ad21e632c73e881e66620a831647
Anne-Mari Kasemetsa <annemari.kasemetsa3@gmail.com>
2024-05-22 18:22:18 +0300

    lae_pilt() meetod

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 from PIL import Image
+
 
 # 1. Sisestad pildi
 # 2. võibolla vaja image töötlust
@@ -16,10 +18,23 @@ from PIL import Image
 #
 # Kui juturobot ei leia täpselt, siis küsib üle, kuni saab õige
 
+#########################################################
+# Pildi üles leidmine ja laadimine
+# sisend: pildinimi (nt "tsekk1.jpg")
+# väljund: Image tüübina pilt
+#########################################################
 def lae_pilt(pildinimi):
-    path = f"tsekid/{pildinimi}"
-    image = Image.open(path)
-    image.show()
+    try:
+        image_path = os.path.join('tsekid', pildinimi)
+        if os.path.isfile(image_path):
+            print(f"Found image {pildinimi}")
+            image = Image.open(image_path)
+            return image
+        else:
+            raise FileNotFoundError(f"No image named '{pildinimi}' found in the '{pildinimi}' directory.")
+    except Exception as e:
+        print(e)
+        return None
 
 
-lae_pilt("tsekk1")
+lae_pilt("tsekk2.jpg")
