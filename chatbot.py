689fb595832e3ca9a069df61fea7d1bba846b06d
stinamaripuu <sinamaripuu@gmail.com>
2024-05-22 16:47:57 +0300

    chatboti input update koos andmetüüpidega

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,3 @@
-# sisend tuleb kujul tooted = [(1, "Kartulikrõps Lays Roheline", 4.77), (2, "Jäätis Limpa", 0.69), (3, "Tualettpabeer Zewa", 12.55)]
+# sisend tuleb kujul
+# tooted = [(jrk nr (int), "nimetus" (str), kogus (int), hind (float)), ()...]
+tooted = [(1, "Kartulikrõps Lays Roheline", 1,4.77), (2, "Jäätis Limpa", 3, 0.69), (3, "Tualettpabeer Zewa", 1, 12.55)]
